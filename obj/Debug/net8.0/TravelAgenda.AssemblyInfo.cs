546648273921234031dfd64c5513b29d430914a5
Ragwar <104670315+Ragwar@users.noreply.github.com>
2025-01-14 21:09:57 +0200

    final_touches

--- obj/Debug/net8.0/TravelAgenda.AssemblyInfo.cs
+++ obj/Debug/net8.0/TravelAgenda.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TravelAgenda")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+680960f1ded0ab5bd36f2380cc87408aa6139c9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd9dbaae8bcad9d41a4627a5b8fc7e19a69a0672")]
 [assembly: System.Reflection.AssemblyProductAttribute("TravelAgenda")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TravelAgenda")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
